c9de52dd7d9c5cebf1926bf48a573abc0e5800bf
andre-stabile <andrestabile2@gmail.com>
2023-09-05 11:31:35 +0200

    Implemented latex output for nonlinear mappings. Improved latex output for reduced dynamics

--- src/MORFE_Symbolic.jl
+++ src/MORFE_Symbolic.jl
@@ -12,7 +12,7 @@ module MORFE_Symbolic
     export parametrisation_struct,init_parametrisation_struct
     export fill_RHS_quad!,fill_RHS_dyn!,fill_RHS_lin!
     export compute_order_zero,generalised_eigenproblem,solve_homological!    
-    export reduced_dynamics_latex_output
+    export reduced_dynamics_latex_output, nonlinear_mappings_latex_output
     
     #~~~~~~~~~~~~~~~~~#
     #           generic vectors       #
@@ -129,6 +129,20 @@ module MORFE_Symbolic
         return part
     end
 
+    """
+    Struct to manage multiexponents. It is constituted of the following:
+        - dic: A dictionary containing pairs of (key, value). There are three kinds of keys:
+            - [deg local_index]      : Used to access the monomial of degree deg with number local_index.
+                                       For example [4, 3] accesses the 3rd monomial of degree 4.
+                                       The returned value is an array, in the format of next item in this list.
+            - [deg_1, ..., deg_n_rom]: Used to access of the monomial z_1^deg_1 * ... * z_n^deg_n_rom,
+                                       where n_rom represents the number of master coordinates.
+                                       The returned value is an integer, the global index of the monomial.
+            - deg                    : Returns an integer, the number of multiexponents of degree deg.
+        - get: A function to get values from dic.
+        - mat: An n_rom x n_sets matrix, in which each column is a multiexponent.
+        - n_sets: The number of monoms with n_rom variables up to order o.
+    """
     mutable struct multiexponent_struct
         get
         dic::Dict{Any,Any}
@@ -136,6 +150,10 @@ module MORFE_Symbolic
         n_sets::Int
     end
 
+    """
+    Initializes the multiexponent struct for a model with n_rom master coordinates and
+    parametrization up to order o.
+    """
     function init_multiexponent_struct(n_rom::Int,o::Int)
         g,d,v,n_sets = create_multiexp(n_rom,o)
         return multiexponent_struct(g,d,v,n_sets)
@@ -475,6 +493,24 @@ module MORFE_Symbolic
     end
 
     """
+    Defines a consistent multiexponent ordering. For multiexponents m1 and m2,
+    if deg1 < deg2, then m1 < m2. If deg1 = deg2, then the comparison is made
+    looking at the degrees of each z_i in order. For example:
+        - z1^3 * z2^4 * z3^1 > z1^3 * z2^3 * z3^3, because the z2 exponent is
+        greater for the first monomial.
+    """
+    function multiexponent_lt(x, y)
+        sum(x) < sum(y) && return true
+        sum(y) < sum(x) && return false
+
+        for i in eachindex(x)
+            x[i] > y[i] && return true    
+            y[i] > x[i] && return false
+        end
+        return false
+    end
+
+    """
     Auxiliary function to transform a given polynomial expression in its latex code. Input arguments are:
         - vars: A symbolic vector containing the variables of the polynomial expression.
         - expr_coeff: The polynomial coefficients.
@@ -483,7 +519,11 @@ module MORFE_Symbolic
     This function is primarely intended for use inside the module, and not for exportation.
     """
     function latex_code_for_polynomial_expression(vars::Vector{Sym}, expr_coeffs::Vector{Sym}, expr_monoms, latex_output::String)
-        for j in reverse(eachindex(expr_coeffs))
+        ordering = sortperm(expr_monoms, lt=multiexponent_lt)
+        expr_coeffs = expr_coeffs[ordering]
+        expr_monoms = expr_monoms[ordering]
+
+        for j in eachindex(expr_coeffs)
             sign_flag = false
             monom = non_commutative_monoms_for_latex_expressions(j, expr_monoms, vars)
             # This sign flag is used as a way to make the i stay outside of fraction numerators
@@ -495,34 +535,49 @@ module MORFE_Symbolic
                 expr_coeffs[j] = sympy.Mul(Sym(im), expr_coeffs[j]/im, evaluate=False)
             end
             # In the next lines the Sym("a") is necessary so that -i is not displayed as \left( -i \right)
-            if sign_flag
-                latex_result = " -"*latexify(sympy.Add(Sym("a"), sympy.Mul(expr_coeffs[j], monom, evaluate=False), evaluate=False), cdot = false)[5:end-1]
+            if expr_coeffs[j] == 1
+                latex_result = latexify(sympy.Add(Sym("a"), monom, evaluate=False), cdot = false)[3:end-1]
             else
                 latex_result = latexify(sympy.Add(Sym("a"), sympy.Mul(expr_coeffs[j], monom, evaluate=False), evaluate=False), cdot = false)[3:end-1]
             end
-            if j == length(expr_coeffs) && !sign_flag
-                latex_output *= latex_result[3:end]
-            else
-                latex_output *= latex_result
+            if sign_flag
+                latex_result = " -"*latex_result[3:end]
             end
+            if j == 1 && !sign_flag
+                latex_result = latex_result[3:end]
+            end
+            latex_output *= latex_result
         end
         return latex_output
     end
 
     """
-    Function to output the reduced dynamics on latex format. Input arguments are:
-        - zₜ: The reduced dynamics. A vector of symbolic expressions.
+    Function to output the reduced dynamics on latex format. Input arguments are:\\
+        - DP: The parametrisation data structure.\\
+        - aexp: A multiexponent_struct defining the numbering of the monoms in the parametrisation.\\
+        - substitutions: An array of dictionaries (or of arrays of dictionaries) containing substitutions
+                         to be made on the symbolic variables.\\
         - output_file: A file to output the results. If it is not passed, results are displayed
-                       on the terminal.
+                       on the terminal.\\
         - file_mode: The opening mode of the file. If "a", the output is appended at the end.
                      If "w" overwrites existing files with the same name.
     """
-    function reduced_dynamics_latex_output(zₜ::Vector{Sym}, output_file = nothing, file_mode::String = "a")
+    function reduced_dynamics_latex_output(DP::parametrisation_struct, aexp::multiexponent_struct, substitutions, output_file = nothing, file_mode::String = "a")
         poly_from_expr = sympy.polys.polytools.poly_from_expr
 
-        z = Sym[]
-        for i = 1:length(zₜ)
-            push!(z, Sym("z_$(i)"))
+        println("Printing reduced dynamics")
+
+        zₜ = sympy.zeros(DP.n_aut,1)[:,1]
+        z = [Sym("z_$(i)") for i=1:DP.n_aut]
+        for i_ord=1:length(DP.f[1,:])
+            monom = prod(z .^ aexp.mat[:,i_ord])
+            for i_var=1:DP.n_aut
+                substituted = mysub([DP.f[i_var,i_ord]],DP.subs[end:-1:1])
+                for substitute in substitutions
+                    substituted = mysub(substituted, substitute)
+                end
+                zₜ[i_var:i_var] += substituted*monom
+            end
         end
         
         latex_output = "\\begin{align}"
@@ -535,17 +590,71 @@ module MORFE_Symbolic
             latex_output *= "\\\\"
         end
         latex_output = replace(latex_output, "I" => "\\mathit{i}")
-        latex_output = latex_output[1:end-2] * "\n\\end{align}"
+        latex_output = latex_output[1:end-2] * "\n\\end{align}\n"
         
         if output_file === nothing
-            println("RHS dynamics whose LHS is zₜ=[∂z₁/∂t  ∂z₂/∂t  ... ]")
+            println("Reduced dynamics:")
             println(latex_output)
         else
             open(output_file, file_mode) do file
-                write(file, "RHS dynamics whose LHS is zₜ=[∂z₁/∂t  ∂z₂/∂t  ... ]\n")
+                write(file, "Reduced dynamics:\n")
                 write(file, latex_output)
             end 
-        end 
+        end
+        println("Reduced dynamics printed")
+    end
+
+    """
+    Function to output the nonlinear mappings on latex format. Input arguments are:\\
+        - DP: The parametrisation data structure.\\
+        - aexp: A multiexponent_struct defining the numbering of the monoms in the parametrisation.\\
+        - substitutions: An array of dictionaries (or of arrays of dictionaries) containing substitutions
+                         to be made on the symbolic variables.\\
+        - output_file: A file to output the results. If it is not passed, results are displayed
+                       on the terminal.\\
+        - file_mode: The opening mode of the file. If "a", the output is appended at the end.
+                     If "w" overwrites existing files with the same name.
+    """
+    function nonlinear_mappings_latex_output(DP::parametrisation_struct, aexp::multiexponent_struct, substitutions, output_file = nothing, file_mode::String = "a")
+        poly_from_expr = sympy.polys.polytools.poly_from_expr
+
+        println("Printing nonlinear mappings")
+
+        u = sympy.zeros(DP.n_aut,1)[:,1]
+        z = [Sym("z_$(i)") for i=1:DP.n_aut]
+        for i_ord=1:length(DP.W[1,:])
+            monom = prod(z .^ aexp.mat[:,i_ord])
+            for i_var=1:DP.n_aut
+                substituted = mysub([DP.W[i_var,i_ord]],DP.subs[end:-1:1])
+                for substitute in substitutions
+                    substituted = mysub(substituted, substitute)
+                end
+                u[i_var:i_var] += substituted*monom
+            end
+        end
+        
+        latex_output = "\\begin{align}"
+        for i in eachindex(u)
+            expr = poly_from_expr(u[i], gens = z)
+            expr_monoms = expr[1].monoms()
+            expr_coeffs = expr[1].coeffs()
+            latex_output *= "\ny_{$(i)} &="
+            latex_output = latex_code_for_polynomial_expression(z, expr_coeffs, expr_monoms, latex_output)
+            latex_output *= "\\\\"
+        end
+        latex_output = replace(latex_output, "I" => "\\mathit{i}")
+        latex_output = latex_output[1:end-2] * "\n\\end{align}\n"
+        
+        if output_file === nothing
+            println("Nonlinear mappings:")
+            println(latex_output)
+        else
+            open(output_file, file_mode) do file
+                write(file, "Nonlinear mappings:\n")
+                write(file, latex_output)
+            end 
+        end
+        println("Nonlinear mappings printed")
     end
 end
 
